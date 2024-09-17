329ea84cfe526741bbb4f4225ece43d7e4d4af15
Sulthan Fawwaz <Sulthan Fawwaz@LAPTOP-Q9IT01RK>
2024-09-17 08:47:45 +0700

    Restaurant Web

--- web/JS/Restoku.js
+++ web/JS/Restoku.js
@@ -1,3 +1,18 @@
+
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  });
 document.addEventListener('DOMContentLoaded', (event) => {
     const datePicker = document.getElementById('datePicker');
     const today = new Date();
